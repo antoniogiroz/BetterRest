9114309404ec1d73cde5a36a79d0a1a619ddbf4a
Antonio Giroz <antonioluisgil@gmail.com>
2025-10-01 15:04:26 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Antonio Giroz on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
